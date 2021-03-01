ee359db0d86ad1c0db2961ba733f011f76c6ee10
Arkegs <silvestre1602@gmail.com>
2021-03-01 22:38:03 +0000

    add material section and visual improvement

--- public/scripts/general.js
+++ public/scripts/general.js
@@ -26,3 +26,11 @@ function disappear(){
 	x.style.display = "none";
 	document.getElementById("hamb").style.backgroundColor = "#e20101"
 }
+
+var channelID = "UCrLxPGAfMYhLRQLuJmR3u2Q";
+var reqURL = "https://www.youtube.com/feeds/videos.xml?channel_id=";
+$.getJSON("https://api.rss2json.com/v1/api.json?rss_url=" + encodeURIComponent(reqURL)+channelID, function(data) {
+   var link = data.items[0].link;
+   var id = link.substr(link.indexOf("=")+1);
+$("#youtube_video").attr("src","https://youtube.com/embed/"+id + "?controls=0&showinfo=0&rel=0");
+});
