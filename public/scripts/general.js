48bf8b2d7315f885551c9e7462d08301be1db1b2
Arkegs <silvestre1602@gmail.com>
2021-03-03 23:39:00 +0000

    finish contact and material section

--- public/scripts/general.js
+++ public/scripts/general.js
@@ -27,6 +27,25 @@ function disappear(){
 	document.getElementById("hamb").style.backgroundColor = "#e20101"
 }
 
+function disappearSongchart(){
+	var x = document.getElementById("songchart");
+	var y = document.getElementsByClassName("songlist");
+	if (x.style.opacity === "1") {
+		x.style.setProperty("-webkit-transition", "opacity 1s");
+		x.style.opacity = 0;
+		setTimeout(function(){
+			y[0].style.display = "none";
+			y[1].style.display = "none";
+		}, 450);
+		
+	} else {
+		y[0].style.display = "block";
+		y[1].style.display = "block";
+		x.style.opacity = 1;
+		x.style.setProperty("-webkit-transition", "opacity 1s");
+	}
+}
+
 var channelID = "UCrLxPGAfMYhLRQLuJmR3u2Q";
 var reqURL = "https://www.youtube.com/feeds/videos.xml?channel_id=";
 $.getJSON("https://api.rss2json.com/v1/api.json?rss_url=" + encodeURIComponent(reqURL)+channelID, function(data) {
